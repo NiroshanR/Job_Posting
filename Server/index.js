446a0b5d343ef95ebb0c3ac940aa52b3eafbd860
NiroshanR <niroshanragu@gmail.com>
2023-12-20 01:20:39 +0530

    added database config and initialization

--- Server/index.js
+++ Server/index.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const dotenv = require('dotenv')
+const mongoose = require('mongoose')
 dotenv.config()
 
 //Creating an application
@@ -10,12 +11,29 @@ const app = express()
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-//Creating an simple GET route
+//Creating a simple health API using a GET route
 app.get('/', (req, res) => {
-    res.send('Server is connected')
+    try{
+        res.json({
+                status: 'SUCCESS',
+                message: 'Server running sucsessfully!'
+            })
+        }catch(error){
+            res.json({
+                status: 'FAILED',
+                message: 'Something went wrong!'
+    
+            })
+        }
 })
+
+
 
 // Attaching the server to a port
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running sucessfully on ${process.env.PORT} `)
+    mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(console.log(`Server is running sucessfully on ${process.env.PORT} `))
+    .catch((error) => console.log(error))
+    
 })
